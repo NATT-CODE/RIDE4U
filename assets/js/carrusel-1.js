f0958d6611f6e235bec9f2defc3c08bf429ab8dc
NATT-CODE <natalyrodriguez.raices2024@gmail.com>
2024-06-26 11:54:51 -0600

    Pagina completa

---
+++ assets/js/carrusel-1.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){ $('.owl-carousel').owlCarousel({
+    loop:true, 
+    margin:10, 
+    responsiveClass: true, 
+    center: true,
+    autoplay: true,
+    delay: 8000,
+    nav: true,
+    navText:[
+        "<i class='fa fa-angle-left'></i>", 
+        "<i class='fa fa-angle-right'></i>"
+    ],
+    
+    responsive:{
+        0:{
+            items:1,
+        },
+        600: {
+            items:3,
+        },
+        1000:{
+            items:3,
+        }
+        }
+        })
+    });
